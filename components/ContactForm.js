0166ea466c20e607885d812e77effad9d6a04943
Lukas <lukassh@gmail.com>
2019-03-20 19:07:16 +0100

    Fix contactForm.js

--- components/ContactForm.js
+++ components/ContactForm.js
@@ -9,7 +9,7 @@ var ContactForm = React.createClass({
         <input type='text' placeholder='Imię' value={'this.props.contact.firstName'}>
         <input type='text' placeholder='Nazwisko' value={'this.props.contact.lastName'}>
         <input type='email' placeholder='Email' value={'this.props.contact.email'}>
-        <button type='submit'>Dodaj kontakt</submit>
+        <button type='submit'>Dodaj kontakt</button>
       </form>
     )
   },
